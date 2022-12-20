5627093f69c1409d3658127eec6cd6052d2020fe
speehive <speehive@DESKTOP-TPF0JOE>
2022-12-20 20:27:28 +0530

    Add project files.

---
+++ Helper/HelperClass.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleRentel.Model;
+
+namespace VehicleRentel.Helper
+{
+    internal class HelperClass
+    {
+        public Car GetCarDetails(Car C)
+        {
+            Console.WriteLine("Enter Car Name:");
+            C.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter model:");
+            C.Model = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Price:");
+            C.Price = Convert.ToInt32(Console.ReadLine());
+            return C;
+
+        }
+
+        public void DisplayCar(Car C)
+        {
+            Console.WriteLine("\nName    :" + C.Name);
+            Console.WriteLine("\nModel   :" + C.Model);
+            Console.WriteLine("\nPrice   :" + C.Price);
+
+        }
+
+
+
+        public Bike GetBikeDetails(Bike B)
+        {
+            Console.WriteLine("Enter Bike Name:");
+            B.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter model:");
+            B.Model = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Price:");
+            B.Price = Convert.ToInt32(Console.ReadLine());
+            return B;
+
+
+        }
+
+        public void DisplayBike(Bike B)
+        {
+            Console.WriteLine("\nName    :" + B.Name);
+            Console.WriteLine("\nModel   :" + B.Model);
+            Console.WriteLine("\nPrice   :" + B.Price);
+
+
+        }
+
+        /* public void WriteAFile(Car C);
+         {
+
+         String path = @"C:\Users\Rinsh\source\repos\VehicleRentel\notepad\" + C.Name + ".txt";
+         StreamWriter ob = new StreamWriter(path, true);
+
+         ob.Write("\nName:   "+C.Name+ "\nModel:     "+C.Model+"\nYear   :"+C.Price);
+         ob.Close();
+         }*/
+
+        public void WriteaFile(Car C , Bike B)
+        {
+            String path = @"C:\Users\Rinsh\source\repos\VehicleRentel\notepad\" + C.Name + ".txt";
+           
+            StreamWriter ob = new StreamWriter(path, true);
+
+
+            ob.Write("\nName  :" + C.Name + "\nModel    :" + C.Model + "\nPrice     :" + C.Price);
+            ob.Write("\nName  :" + B.Name + "\nModel    :" + B.Model + "\nPrice     :" + B.Price);
+            ob.Close();
+            Process.Start("notepad.exe", @"C:\Users\Rinsh\source\repos\VehicleRentel\notepad\" + C.Name + ".txt");
+           
+        }
+
+        
+
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleRentel.Helper;
+using VehicleRentel.Model;
+using System.Diagnostics;
+
+namespace VehicleRentel
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Car C= new Car();
+            Bike B = new Bike();
+            HelperClass help = new HelperClass();
+
+           Car C1= help.GetCarDetails(C);
+            Bike B1=help.GetBikeDetails(B);
+
+            help.DisplayCar(C1);
+            help.DisplayBike(B1);
+            help.WriteaFile(C , B);
+            //Process.Start("notepad.exe", @"C:\Users\Rinsh\source\repos\VehicleRentel\notepad\" + C.Name + ".txt");
+
+        }
+    }
+}
+ 
